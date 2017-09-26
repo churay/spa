4029cd2ddd1f377d9d933eb3498641b91ef8f87c
Joseph Ciurej <churayy@gmail.com>
2017-09-25 19:54:03 -0700

    Initial setup for the 'spa' project.

---
+++ spa_v1/spa.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+__doc__ = '''Module for "SPA" Console Application'''
+
+import os, optparse
+import PIL
+
+### Main Entry Point ###
+
+def main():
+    ## Argument Parsing ##
+
+    print 'TODO(JRC): Write the application code here.'
+
+### Miscellaneous ###
+
+if __name__ == '__main__':
+    main()
