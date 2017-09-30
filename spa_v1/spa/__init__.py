238ece5626e44aa03b1c3dafe9401c7ca25fe9bc
Joseph Ciurej <churayy@gmail.com>
2017-09-29 22:47:07 -0700

    Added a dedicated module for the project with a disjoint set implementation.

---
+++ spa_v1/spa/__init__.py
@@ -0,0 +1,1 @@
+from dsets import dsets
---
+++ spa_v1/spa/dsets.py
@@ -0,0 +1,50 @@
+__doc__ = '''Module for the Disjoint Set Structure Implementation'''
+
+import collections
+
+### Module Classes ###
+
+class dsets():
+    ### Constructors ###
+
+    def __init__(self, size):
+        self._dsets = [-1] * size
+
+    ### Operators ###
+
+    def __repr__(self):
+        return str(self.tabulate())
+
+    ### Methods ###
+
+    def union(self, dset1, dset2):
+        dset1, dset2 = tuple(self.find(d) for d in (dset1, dset2))
+        dsize1, dsize2 = tuple(self._dsets[d] for d in (dset1, dset2))
+
+        if abs(dsize1) >= abs(dsize2):
+            self._dsets[dset1] = dsize1 + dsize2
+            self._dsets[dset2] = dset1
+        else:
+            self._dsets[dset1] = dset2
+            self._dsets[dset2] = dsize1 + dsize2
+
+    def find(self, dset):
+        if self._is_delegate(dset):
+            return dset
+        else:
+            self._dsets[dset] = self.find(self._dsets[dset])
+            return self._dsets[dset]
+
+    def length(self):
+        return sum((int(self._is_delegate(d)) for d in range(len(self._dsets))), 0)
+
+    def tabulate(self):
+        table = collections.defaultdict(list)
+        for dset in range(len(self._dsets)):
+            table[self.find(dset)].append(dset)
+        return table
+
+    ### Helpers ###
+
+    def _is_delegate(self, dset):
+        return self._dsets[dset] < 0
