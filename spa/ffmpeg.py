f66d3989e6d3faee6374fb0b2fe9471e0dfaba0e
Joseph Ciurej <churayy@gmail.com>
2018-04-24 20:32:35 -0700

    Adding a better half-working version of GIF conversion.

--- spa/ffmpeg.py
+++ spa/ffmpeg.py
@@ -24,18 +24,17 @@ def ffmpeg(path, args, quality=0):
     ffmpeg_args = ['ffmpeg']
 
     ffmpeg_args.extend(args)
-    ffmpeg_args.extend(['-r', 60, '-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-y'])
-    ffmpeg_args.extend(['-crf', 0 if quality == 1 else 22])
+    ffmpeg_args.extend(['-r', '60', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-y'])
+    ffmpeg_args.extend(['-crf', '0' if quality == 1 else '22'])
     ffmpeg_args.append(path)
 
-    ffmpeg_args = map(str, ffmpeg_args)
     # spa.log.debug(' '.join(ffmpeg_args))
     subprocess.check_output(ffmpeg_args, stderr=subprocess.STDOUT)
 
 def encode(path, out_encoding, fps=60.0, quality=0):
-    # NOTE(JRC): The code below was adapted from the tutorial here:
-    # http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
     if out_encoding == encoding.gif:
+        # NOTE(JRC): THe code below was adapted from the SO answer here:
+        # https://askubuntu.com/a/648604/285545
         filter_flags = 'fps={0}'.format(fps)
 
         path_dir, path_base = os.path.dirname(path), os.path.basename(path)
@@ -45,8 +44,23 @@ def encode(path, out_encoding, fps=60.0, quality=0):
 
         os.rename(path, temp_path)
 
+        encode_args = ['ffmpeg']
+        encode_args.extend(['-i', temp_path])
+        encode_args.extend(['-r', '50', '-f', 'image2pipe', '-vcodec', 'ppm'])
+        encode_args.extend(['-', '|'])
+        encode_args.extend(['convert', '-delay', '2', '-loop', '0', '-', path])
+
+        # spa.log.debug(' '.join(encode_args))
+        subprocess.check_output(' '.join(encode_args), stderr=subprocess.STDOUT, shell=True)
+
+        # TODO(JRC): Debug and re-enable the code below since it doesn't
+        # require the use of the 'convert' utility.
+        '''
+        # NOTE(JRC): The code below was adapted from the tutorial here:
+        # http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
         ffmpeg_args = ['ffmpeg']
         ffmpeg_args.extend(['-i', temp_path])
+        ffmpeg_args.extend(['-r', '60', '-framerate', str(fps)])
         ffmpeg_args.extend(['-vf', '{0},palettegen'.format(filter_flags), '-y'])
         ffmpeg_args.append(palette_path)
 
@@ -55,20 +69,22 @@ def encode(path, out_encoding, fps=60.0, quality=0):
 
         ffmpeg_args = ['ffmpeg']
         ffmpeg_args.extend(['-i', temp_path, '-i', palette_path])
+        ffmpeg_args.extend(['-r', '60', '-framerate', str(fps)])
         ffmpeg_args.extend(['-lavfi', '{0}[x];[x][1:v]paletteuse'.format(filter_flags), '-y'])
         ffmpeg_args.append(path)
 
         # spa.log.debug(' '.join(ffmpeg_args))
         subprocess.check_output(ffmpeg_args, stderr=subprocess.STDOUT)
+        '''
 
         # TODO(JRC): Adapt this code so that these files are cleaned up
         # even if there are errors during processing above.
-        os.remove(palette_path)
+        # os.remove(palette_path)
         os.remove(temp_path)
 
 def render(path, template, fps=60.0, quality=0):
     render_args = [
-        '-framerate', fps,
+        '-framerate', str(fps),
         '-i', template,
     ]
 
