24d6490e92ccbeb87926ee2f86a10c131c4b5627
Joseph Ciurej <churayy@gmail.com>
2018-08-22 23:57:46 -0700

    Added a very quick Bezier implementation to help with interpolation.

--- spa/__init__.py
+++ spa/__init__.py
@@ -2,5 +2,6 @@ from spa import *
 
 from movie import movie
 from vector import vector
+from bezier import bezier
 
 import imp, fx, ffmpeg
---
+++ spa/bezier.py
@@ -0,0 +1,41 @@
+__doc__ = '''Module for the Bezier Function Implementation'''
+
+import math
+import vector
+
+### Module Classes ###
+
+class bezier(object):
+    ### Constructors ###
+
+    def __init__(self, *args):
+        assert len(args) % 2 == 0, \
+            'Improperly defined Bezier curve.'
+        assert all(ap < an for ap, an in zip(args[:-2:2], args[2::2])), \
+            'Improperly defined Bezier curve.'
+
+        self._cpoints = [0.0, 0.0] + list(args) + [1.0, 1.0]
+        self._cpoints = [vector.vector(2, cx, cy) for cx, cy 
+            in zip(self._cpoints[:-1:2], self._cpoints[1::2])]
+        self._degree = len(self._cpoints) - 1
+
+    ### Properties ###
+
+    @property
+    def degree(self):
+        return self._degree
+
+    ### Methods ###
+
+    def evaluate(self, t):
+        assert 0.0 <= t <= 1.0, 'Invalid evaluation parameter; must be in [0, 1].'
+
+        # TODO(JRC): This was ripped directly from Wikipedia, so some semblance
+        # of actual knowledge needs to be embedded into these variables and their
+        # evaluation (perhaps using De-Castlejau instead).
+        evals = [1.0 * v for v in self._cpoints]
+        for k in range(1, len(self._cpoints) + 1):
+            for i in range(len(self._cpoints) - k):
+                evals[i] = (1.0 - t) * evals[i] + t * evals[i + 1]
+
+        return evals[0][1]
